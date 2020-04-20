4de155446b83fdd59bb7b5e9821d06c2ff27da4a
cammylim <64000790+cammylim@users.noreply.github.com>
2020-04-20 21:57:45 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,11 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter integer: ");
+    int i1 = in.nextInt();
+    int result = i1*i1;
+    String reply = String.format("Your calculated integer is: %d",result);
+    System.out.print(reply);
   }
 }
