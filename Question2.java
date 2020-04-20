b1d39f900f10eb305ede7bf9b713dc935bba9858
cammylim <64000790+cammylim@users.noreply.github.com>
2020-04-20 21:52:19 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-
-public class Question2
-{
-  public static void main(String[] args)
-  {
-    /**
-     * Prompts user for height and weight, calculate and print out the height and weight of the user
-     * 
-     * Hint: in.nextDouble() to read double
-     */
-     
+class Main {
+  public static void main(String[] args) {
+    
     Scanner in = new Scanner(System.in);
+    System.out.print("Enter your height: ");
     double height = in.nextDouble();
+    System.out.print("Enter your weight: ");
+    double weight = in.nextDouble();
+    String h1 = String.format("Your height is: %.2f",height);
+    String h2 = String.format("Your weight is: %.2f",weight);
+    System.out.print(h1);
+    System.out.print(h2);
   }
 }
