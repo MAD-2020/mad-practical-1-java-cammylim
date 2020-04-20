e3b7102d8aa2ef86cf7b7ec6060a8e86202c6ae1
cammylim <64000790+cammylim@users.noreply.github.com>
2020-04-20 22:36:18 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,15 +1,29 @@
 import java.util.Scanner;
 
-public class Question4
+public class Question5
 {
   public static void main(String[] args)
   {
     Scanner in = new Scanner(System.in);
-    System.out.print("Triangle base: ");
-    int base = in.nextInt();
-    String str = "*";
-    for (int i = 0; i < base; i++){
-      System.out.println(str.repeat(base-i));
+    System.out.print("Enter integer: ");
+    int i1 = in.nextInt();
+    int[] a = new int[i1];
+    int max = 0;
+    int modevalue = 0;
+    for (int i = 0; i < i1; i++){
+      int i2 = in.nextInt();
+      a[i] = i2;
+      int count = 0;
+      for (int j = 0; j<a.length; j++){
+        if (a[j] == a[i]){
+        count+=1;
+        }
+        if (count > max){
+          max = count;
+          modevalue = a[i];
+        }
+      }
     }
+    System.out.print(modevalue);
   }
 }
